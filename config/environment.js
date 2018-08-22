a84dfd6502315284e78e72b40e1a122eb64ce505
nasiratt92 <nasiratt92@gmail.com>
2018-08-22 12:35:41 +0100

    Updated mongo DB URI

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,6 @@
 const port = process.env.PORT || 4000;
 const env = process.env.NODE_ENV || 'dev'; //so far we got production or developement
-const dbURI = process.env.DB_URI || `mongodb://localhost/sinatra-${env}`;
+const dbURI = process.env.MONGODB_URI || `mongodb://localhost/sinatra-${env}`;
 const secret = process.env.SECRET || 'blueeyes';
 const ticketmasterApiKey = process.env.TM_API_KEY;
 module.exports = { port, dbURI, secret, ticketmasterApiKey };
